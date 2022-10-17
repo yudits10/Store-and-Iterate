e4ed89535c440eabf6c1fe242dbc204957ee2255
yudits10 <113966685+yudits10@users.noreply.github.com>
2022-10-18 04:14:37 +0700

    Add files via upload

---
+++ Store and Iterate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StoreAndIterate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+            foreach (string orderID in orderIDs)
+            {
+                if (orderID.StartsWith("B"))
+                {
+                    Console.WriteLine(orderID);
+                }
+            }
+        }
+    }
+}
